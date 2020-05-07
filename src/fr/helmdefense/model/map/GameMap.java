d36e45c398d03af7c5349c2f1da892163b5be05d
indyteo <theo.mail@laposte.net>
2020-05-07 02:01:55 +0200

    Created GameMap with tiles int array

---
+++ src/fr/helmdefense/model/map/GameMap.java
@@ -0,0 +1,18 @@
+package fr.helmdefense.model.map;
+
+public class GameMap {
+private int[][] tiles;
+	
+	public static final int WIDTH = 10;
+	public static final int HEIGHT = 10;
+	
+	public GameMap(int[][] tiles) {
+		this.tiles = tiles;
+	}
+	
+	public int getTile(int x, int y) {
+		if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+			throw new OutOfMapException();
+		return this.tiles[y][x];
+	}
+}
