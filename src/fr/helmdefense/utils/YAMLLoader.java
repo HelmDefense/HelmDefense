d67dc4d441c33e01ffb40f375375f9c7da4b18c2
indyteo <theo.mail@laposte.net>
2020-05-07 17:00:14 +0200

    Added loadStats(String name) method to load stats with a given entity path

--- src/fr/helmdefense/utils/YAMLLoader.java
+++ src/fr/helmdefense/utils/YAMLLoader.java
@@ -3,17 +3,23 @@ package fr.helmdefense.utils;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
 
+import fr.helmdefense.model.entities.utils.Statistic;
 import fr.helmdefense.model.level.Level;
 import fr.helmdefense.model.map.GameMap;
 
 public class YAMLLoader {
+	private static Map<String, ?> stats = null;
 	private static final Yaml YAML = new Yaml();
 	
 	public static final String SAVES_FOLDER = "saves";
+	public static final String DATA_FOLDER = "game_data";
 	
 	private YAMLLoader() {}
 	
@@ -37,5 +43,75 @@ public class YAMLLoader {
 			e.printStackTrace();
 			throw new YAMLLoadException();
 		}
+	}
+
+	public static Statistic loadStats(String name) {
+		if (stats == null)
+			loadStats();
+		
+		Object obj = path(stats, name);
+		if (obj == null || ! (obj instanceof Map))
+			throw new YAMLLoadException("Incorrect format for entity!");
+		Map<?, ?> s = (Map<?, ?>) obj;
+		
+		try {
+			return new Statistic(
+					toInt(s.get("hp")),
+					toInt(s.get("dmg")),
+					toDouble(s.get("mvt-spd")),
+					toDouble(s.get("atk-spd")),
+					toDouble(s.get("atk-range")),
+					toDouble(s.get("dist-range"))
+			);
+		} catch (ClassCastException e) {
+			throw new YAMLLoadException("Incorrect format for entity!", e);
+		}
+	}
+	
+	private static void loadStats() {
+		try {
+			stats = YAML.load(new FileReader(Paths.get(DATA_FOLDER, "entity_stats.yml").toString()));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new YAMLLoadException("Cannot find \"entity_stats.yml\" file in folder \"" + DATA_FOLDER + "\"");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new YAMLLoadException();
+		}
+	}
+	
+	public static Object path(Map<?, ?> data, String path, Object def) {
+		List<String> layers = new ArrayList<String>(Arrays.asList(path.split("\\.")));
+		String wanted = layers.remove(layers.size() - 1);
+		Map<?, ?> actual = data;
+		for (String layer : layers) {
+			Object o = actual.get(layer);
+			if (o != null && o instanceof Map)
+				actual = (Map<?, ?>) o;
+			else
+				return def;
+		}
+		
+		return actual.containsKey(wanted) ? actual.get(wanted) : def;
+	}
+	
+	public static Object path(Map<?, ?> data, String path) {
+		return path(data, path, null);
+	}
+	
+	public static int toInt(Object obj, int def) {
+		return obj == null ? def : ((Number) obj).intValue();
+	}
+	
+	public static int toInt(Object obj) {
+		return toInt(obj, -1);
+	}
+	
+	public static double toDouble(Object obj, double def) {
+		return obj == null ? def : ((Number) obj).doubleValue();
+	}
+	
+	public static double toDouble(Object obj) {
+		return toDouble(obj, -1);
 	}
 }
