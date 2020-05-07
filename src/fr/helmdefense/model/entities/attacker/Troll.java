d334df74abb5ace28fa04b34187e320cef86e71f
JulesPopo <jpommery1@gmail.com>
2020-05-07 10:25:35 +0200

    renaming Trolls.java to Troll.java

---
+++ src/fr/helmdefense/model/entities/attacker/Troll.java
@@ -0,0 +1,11 @@
+package fr.helmdefense.model.entities.attacker;
+
+import fr.helmdefense.model.entities.Entity;
+
+public class Troll extends Entity {
+
+	public Troll(int x, int y) {
+		super(x, y, 25000, 500, 16, 0.5, 64, 0);
+	}
+
+}
