87cfecf754b5f1f63c5704fcabac34ff5a7ac2eb
indyteo <theo.mail@laposte.net>
2020-05-10 15:39:07 +0200

    Modified methods to use YAMLData

--- src/fr/helmdefense/utils/YAMLLoader.java
+++ src/fr/helmdefense/utils/YAMLLoader.java
@@ -3,10 +3,7 @@ package fr.helmdefense.utils;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
 
@@ -15,7 +12,7 @@ import fr.helmdefense.model.level.Level;
 import fr.helmdefense.model.map.GameMap;
 
 public class YAMLLoader {
-	private static Map<String, ?> stats = null;
+	private static YAMLData entities = null;
 	private static final Yaml YAML = new Yaml();
 	
 	public static final String SAVES_FOLDER = "saves";
@@ -28,90 +25,39 @@ public class YAMLLoader {
 	}
 	
 	private static GameMap loadMap(String name) {
-		try {
-			List<List<Integer>> map = YAML.load(new FileReader(Paths.get(SAVES_FOLDER, name, "map.yml").toString()));
-			int[][] tiles = new int[map.size()][map.get(0).size()];
-			for (int i = 0; i < map.size(); i++)
-				for (int j = 0; j < map.get(i).size(); j++)
-					tiles[i][j] = map.get(i).get(j);
-			
-			return new GameMap(tiles);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new YAMLLoadException("Cannot find map file for level \"" + name + "\"");
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new YAMLLoadException();
-		}
+		List<List<Integer>> map = load(Paths.get(SAVES_FOLDER, name, "data.yml").toString()).get("map");
+		int[][] tiles = new int[map.size()][map.get(0).size()];
+		for (int i = 0; i < map.size(); i++)
+			for (int j = 0; j < map.get(i).size(); j++)
+				tiles[i][j] = map.get(i).get(j);
+		
+		return new GameMap(tiles);
 	}
 
 	public static Statistic loadStats(String name) {
-		if (stats == null)
-			loadStats();
+		if (entities == null)
+			entities = load(Paths.get(DATA_FOLDER, "entities.yml").toString());
 		
-		Object obj = path(stats, name);
-		if (obj == null || ! (obj instanceof Map))
+		YAMLData s = entities.getData(name);
+		if (s == null)
 			throw new YAMLLoadException("Incorrect format for entity!");
-		Map<?, ?> s = (Map<?, ?>) obj;
 		
-		try {
-			return new Statistic(
-					toInt(s.get("hp")),
-					toInt(s.get("dmg")),
-					toDouble(s.get("mvt-spd")),
-					toDouble(s.get("atk-spd")),
-					toDouble(s.get("atk-range")),
-					toDouble(s.get("dist-range"))
-			);
-		} catch (ClassCastException e) {
-			throw new YAMLLoadException("Incorrect format for entity!", e);
-		}
+		return new Statistic(
+				s.getInt("hp"),
+				s.getInt("dmg"),
+				s.getDouble("mvt-spd"),
+				s.getDouble("atk-spd"),
+				s.getDouble("atk-range"),
+				s.getDouble("dist-range")
+		);
 	}
 	
-	private static void loadStats() {
+	public static YAMLData load(String file) {
 		try {
-			stats = YAML.load(new FileReader(Paths.get(DATA_FOLDER, "entity_stats.yml").toString()));
+			return new YAMLData(YAML.load(new FileReader(file)));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-			throw new YAMLLoadException("Cannot find \"entity_stats.yml\" file in folder \"" + DATA_FOLDER + "\"");
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new YAMLLoadException();
+			throw new YAMLLoadException("Cannot find file \"" + file + "\"!", e);
 		}
-	}
-	
-	public static Object path(Map<?, ?> data, String path, Object def) {
-		List<String> layers = new ArrayList<String>(Arrays.asList(path.split("\\.")));
-		String wanted = layers.remove(layers.size() - 1);
-		Map<?, ?> actual = data;
-		for (String layer : layers) {
-			Object o = actual.get(layer);
-			if (o != null && o instanceof Map)
-				actual = (Map<?, ?>) o;
-			else
-				return def;
-		}
-		
-		return actual.containsKey(wanted) ? actual.get(wanted) : def;
-	}
-	
-	public static Object path(Map<?, ?> data, String path) {
-		return path(data, path, null);
-	}
-	
-	public static int toInt(Object obj, int def) {
-		return obj == null ? def : ((Number) obj).intValue();
-	}
-	
-	public static int toInt(Object obj) {
-		return toInt(obj, -1);
-	}
-	
-	public static double toDouble(Object obj, double def) {
-		return obj == null ? def : ((Number) obj).doubleValue();
-	}
-	
-	public static double toDouble(Object obj) {
-		return toDouble(obj, -1);
 	}
 }
