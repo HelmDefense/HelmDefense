c5bfcdc6375eb7905c213a6f7d7f4a48fa4e0f3d
indyteo <theo.mail@laposte.net>
2020-05-13 17:39:10 +0200

    Added RandomMoveAbility to attackers

--- src/fr/helmdefense/model/entities/attackers/Attacker.java
+++ src/fr/helmdefense/model/entities/attackers/Attacker.java
@@ -1,12 +1,16 @@
 package fr.helmdefense.model.entities.attackers;
 
 import fr.helmdefense.model.entities.Entity;
+import fr.helmdefense.model.entities.abilities.list.RandomMoveAbility;
 import fr.helmdefense.model.entities.utils.Location;
+import fr.helmdefense.model.entities.utils.Tier;
 
 public abstract class Attacker extends Entity {
 
 	public Attacker(Location loc) {
 		super(loc);
+		
+		this.addAbilities(new RandomMoveAbility(Tier.TIER_0, this));
 	}
 
 	public Attacker(int x, int y) {
