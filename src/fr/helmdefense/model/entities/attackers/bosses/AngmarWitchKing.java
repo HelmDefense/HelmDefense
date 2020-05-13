ea20adc32426d0bddc07faace9b87ba2b1f99f21
indyteo <theo.mail@laposte.net>
2020-05-13 23:45:50 +0200

    Refactored Abilities (moved to yml file)

--- src/fr/helmdefense/model/entities/attackers/bosses/AngmarWitchKing.java
+++ src/fr/helmdefense/model/entities/attackers/bosses/AngmarWitchKing.java
@@ -3,12 +3,18 @@ package fr.helmdefense.model.entities.attackers.bosses;
 import fr.helmdefense.model.entities.utils.Location;
 
 public class AngmarWitchKing extends Boss {
+	private Nazgul nazgul;
 		
 	public AngmarWitchKing(Location location) {
 		super(location);
+		this.nazgul = new Nazgul(location);
 	}
 	
 	public AngmarWitchKing(int x, int y) {
 		this(new Location(x, y));
+	}
+	
+	public Nazgul getNazgul() {
+		return this.nazgul.isAlive() ? this.nazgul : null;
 	}
 }
