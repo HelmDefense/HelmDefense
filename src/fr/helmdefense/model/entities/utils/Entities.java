106579225e0280a7d3ed1dda2410cb4159639fd5
TelioBOT <teliobot@gmail.com>
2020-05-13 09:41:35 +0200

    Optimised getClass(String) utils method

--- src/fr/helmdefense/model/entities/utils/Entities.java
+++ src/fr/helmdefense/model/entities/utils/Entities.java
@@ -2,21 +2,29 @@ package fr.helmdefense.model.entities.utils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import fr.helmdefense.model.entities.Entity;
 
 public class Entities {
 	
-	private static Map<Class<? extends Entity>, Statistic> entityData = new HashMap<Class<? extends Entity>, Statistic>();
+	private static Map<Class<? extends Entity>, EntityData> entityData = YAMLLoader.loadEntityData();
 	
 	private Entities() {}
 	
-	public static Statistic getData(Class<? extends Entity> type) {
+	public static EntityData getData(Class<? extends Entity> type) {
 		return entityData.get(type);
 	}
 	
-	public static void registerData(Class<? extends Entity> type, Statistic stat) {
-		entityData.put(type, stat);
+	public static void registerData(Class<? extends Entity> type, EntityData data) {
+		entityData.put(type, data);
 	}
+	
+	public static Class<? extends Entity> getClass(String path) {
+		return entityData.entrySet().stream()
+				.filter(e -> e.getValue().getPath().equalsIgnoreCase(path))
+				.;
+	}
+	
 	
 }
