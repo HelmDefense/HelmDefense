32111e520d0548e6bc51754306003e40a9c4bc44
indyteo <theo.mail@laposte.net>
2020-05-13 23:45:09 +0200

    Added List<Ability> to data

--- src/fr/helmdefense/model/entities/utils/EntityData.java
+++ src/fr/helmdefense/model/entities/utils/EntityData.java
@@ -1,16 +1,24 @@
 package fr.helmdefense.model.entities.utils;
 
+import java.util.List;
 import java.util.Map;
+
+import fr.helmdefense.model.actions.Action;
+import fr.helmdefense.model.actions.utils.Actions;
+import fr.helmdefense.model.entities.abilities.Ability;
 
 public class EntityData {
 	private String name;
 	private String path;
 	private Map<Tier, Statistic> stats;
+	private List<? extends Ability> abilities;
 	
-	public EntityData(String name, String path, Map<Tier, Statistic> stats) {
+	public EntityData(String name, String path, Map<Tier, Statistic> stats, List<? extends Ability> abilities) {
 		this.name = name;
 		this.path = path;
 		this.stats = stats;
+		this.abilities = abilities;
+		Actions.registerListeners(abilities);
 	}
 	
 	public final String getName() {
@@ -23,5 +31,14 @@ public class EntityData {
 	
 	public final Statistic getStats(Tier tier) {
 		return this.stats.get(tier);
+	}
+	
+	public final void triggerAbilities(Action action) {
+		Actions.trigger(action, this.abilities);
+	}
+
+	@Override
+	public String toString() {
+		return "EntityData [name=" + name + ", path=" + path + ", stats=" + stats + ", abilities=" + abilities + "]";
 	}
 }
