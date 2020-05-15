daa6c59098363819f52b56c6576a6d163f87465f
JulesPopo <jpommery1@gmail.com>
2020-05-15 14:49:55 +0200

    initialized buying system with basic methods

--- src/fr/helmdefense/model/level/Level.java
+++ src/fr/helmdefense/model/level/Level.java
@@ -1,6 +1,7 @@
 package fr.helmdefense.model.level;
 
 import java.util.List;
+import java.util.function.Function;
 
 import fr.helmdefense.model.actions.entity.EntitySpawnAction;
 import fr.helmdefense.model.actions.game.GameTickAction;
@@ -8,6 +9,10 @@ import fr.helmdefense.model.actions.utils.Actions;
 import fr.helmdefense.model.entities.Entity;
 import fr.helmdefense.model.map.GameMap;
 import fr.helmdefense.utils.YAMLLoader;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -18,6 +23,7 @@ public class Level {
 	private List<Wave> waves;
 	private GameLoop gameloop;
 	private Inventory inv;
+	private IntegerProperty purseProperty;
 	
 	public Level(GameMap map, List<Wave> waves) {
 		this.map = map;
@@ -34,6 +40,7 @@ public class Level {
 			Actions.trigger(new GameTickAction(this, ticks));
 		});
 		this.inv = new Inventory();
+		this.purseProperty = new SimpleIntegerProperty(/*this.getStartMoney()*/);
 	}
 	
 	public void startLoop() {
@@ -64,5 +71,33 @@ public class Level {
 
 	public static Level load(String name) {
 		return YAMLLoader.loadLevel(name);
+	}
+	
+	public boolean overdrawn() {
+		return this.getPurseValue() > 0;
+	}
+	
+	public int getPurseValue() {
+		return this.purseProperty.getValue();
+	}
+	
+	// return false if overdrawn, true if money have been debited successfully
+	public boolean debit(int value) {
+		if ( Math.max(this.getPurseValue(), this.getPurseValue() - value) < value )
+			return false;
+		this.purseProperty.setValue(this.getPurseValue() - value);
+		return true;
+	}
+	
+	public void earnCoins(int value) {
+		this.purseProperty.setValue(this.getPurseValue() + value);
+	}
+	
+	public void bindPurse(Property<? super Number> property, Function<IntegerProperty, ObservableValue<Number>> transform) {
+		property.bind(transform != null ? transform.apply(this.purseProperty) : this.purseProperty);
+	}
+	
+	public void bindPurse(Property<? super Number> property) {
+		this.bindPurse(property, null);
 	}
 }
