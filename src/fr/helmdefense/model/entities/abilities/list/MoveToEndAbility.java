6df139f1a06fc5f9d3efe510fa94daa3b6891030
indyteo <theo.mail@laposte.net>
2020-05-18 13:54:00 +0200

    Exploited Graph's BFS' results to calculate move

--- src/fr/helmdefense/model/entities/abilities/list/MoveToEndAbility.java
+++ src/fr/helmdefense/model/entities/abilities/list/MoveToEndAbility.java
@@ -1,44 +1,41 @@
 package fr.helmdefense.model.entities.abilities.list;
 
-import java.util.Random;
-
 import fr.helmdefense.model.actions.ActionHandler;
 import fr.helmdefense.model.actions.entity.EntitySpawnAction;
 import fr.helmdefense.model.actions.game.GameTickAction;
 import fr.helmdefense.model.entities.Entity;
 import fr.helmdefense.model.entities.abilities.Ability;
 import fr.helmdefense.model.entities.utils.Tier;
+import fr.helmdefense.model.entities.utils.coords.Location;
+import fr.helmdefense.model.entities.utils.coords.Vector;
 import fr.helmdefense.model.level.GameLoop;
-import fr.helmdefense.model.map.GameMap;
+import fr.helmdefense.model.map.Cell;
 
-public class RandomMoveAbility extends Ability {
+public class MoveToEndAbility extends Ability {
 	private Entity entity;
+	private Cell movingTo;
 	
-	public RandomMoveAbility(Tier unlock) {
+	public MoveToEndAbility(Tier unlock) {
 		super(unlock);
 	}
 	
 	@ActionHandler
 	public void onSpawn(EntitySpawnAction action) {
 		this.entity = action.getEntity();
+		this.movingTo = this.entity.getLevel().getMap().getGraph().getCellAt(this.entity.getLoc()).getNext();
 	}
 	
 	@ActionHandler
 	public void onTick(GameTickAction action) {
-		if (this.entity == null)
+		if (this.entity == null || this.movingTo == null)
 			return;
 		
-		this.entity.teleport(this.entity.getLoc()
-				.add(randomMove(), randomMove()));
-		if (! this.entity.getLoc().isInMap())
-			this.entity.teleport(GameMap.WIDTH / 2d, GameMap.HEIGHT / 2d);
-	}
-	
-	private double randomMove() {
-		return randomFactor() * this.entity.data().getStats(Tier.TIER_1).getMvtSpd() * GameLoop.TPS;
-	}
-	
-	private int randomFactor() {
-		return new Random().nextInt(3) - 1;
+		Location loc = this.entity.getLoc(), l = this.movingTo.getLoc().center();
+		Vector v = new Vector(loc, l);
+		double d = l.distance(loc);
+		v.multiply(this.entity.data().getStats(Tier.TIER_1).getMvtSpd() / GameLoop.TPS / d);
+		this.entity.teleport(loc.add(v));
+		if (v.length() >= d)
+			this.movingTo = this.movingTo.getNext();
 	}
 }
