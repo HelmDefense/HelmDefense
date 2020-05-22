41fd599ab6c6ae4e2787982bd0a552ca912d7771
indyteo <theo.mail@laposte.net>
2020-05-22 02:16:24 +0200

    Make units extends LivingEntity

--- src/fr/helmdefense/model/entities/defenders/Defender.java
+++ src/fr/helmdefense/model/entities/defenders/Defender.java
@@ -1,9 +1,9 @@
 package fr.helmdefense.model.entities.defenders;
 
-import fr.helmdefense.model.entities.Entity;
+import fr.helmdefense.model.entities.LivingEntity;
 import fr.helmdefense.model.entities.utils.coords.Location;
 
-public abstract class Defender extends Entity {
+public abstract class Defender extends LivingEntity {
 	
 	public Defender(Location loc) {
 		super(loc);
