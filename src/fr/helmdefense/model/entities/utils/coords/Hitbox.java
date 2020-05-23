0d5678a9b344421e804b6d7dfc2082fd57e43ff7
indyteo <theo.mail@laposte.net>
2020-05-23 22:11:26 +0200

    Refactored Hitbox with size & securized location

--- src/fr/helmdefense/model/entities/utils/coords/Hitbox.java
+++ src/fr/helmdefense/model/entities/utils/coords/Hitbox.java
@@ -2,13 +2,17 @@ package fr.helmdefense.model.entities.utils.coords;
 
 public class Hitbox implements Coords {
 	private Location center;
-	private double width;
-	private double height;
+	private Size size;
+	private boolean locationLocked;
+	
+	public Hitbox(Location center, Size size) {
+		this.center = center;
+		this.size = size;
+		this.locationLocked = false;
+	}
 	
 	public Hitbox(Location center, double width, double height) {
-		this.center = center;
-		this.width = width;
-		this.height = height;
+		this(center, new Size(width, height));
 	}
 	
 	public Hitbox(double minX, double minY, double maxX, double maxY) {
@@ -19,26 +23,29 @@ public class Hitbox implements Coords {
 		this(min.getX(), min.getY(), max.getX(), max.getY());
 	}
 	
+	public void lockLocation() {
+		this.locationLocked = true;
+	}
+	
 	public boolean overlaps(Hitbox other) {
 		return this.minX() < other.maxX() && this.maxX() > other.minX()
 				&& this.minY() < other.maxY() && this.maxY() > other.minY();
 	}
 	
 	public boolean contains(double x, double y) {
-		return x >= this.minX() && x < this.maxX()
-                && y >= this.minY() && y < this.maxY();
+		return x >= this.minX() && x < this.maxX() && y >= this.minY() && y < this.maxY();
 	}
 	
 	public boolean contains(Coords coords) {
 		return this.contains(coords.getX(), coords.getY());
 	}
 	
-	public double getVolume() {
-		return this.width * this.height;
+	public Location getCenter() {
+		return this.locationLocked ? this.center.copy() : this.center;
 	}
 	
-	public Location getCenter() {
-		return this.center;
+	public boolean isLocationLocked() {
+		return this.locationLocked;
 	}
 	
 	@Override
@@ -52,59 +59,63 @@ public class Hitbox implements Coords {
 	}
 	
 	public double minX() {
-		return this.center.getX() - this.width / 2;
+		return this.center.getX() - this.size.getWidth() / 2;
 	}
 	
 	public double minY() {
-		return this.center.getY() - this.height / 2;
+		return this.center.getY() - this.size.getHeight() / 2;
 	}
 	
 	public double maxX() {
-		return this.center.getX() + this.width / 2;
+		return this.center.getX() + this.size.getWidth() / 2;
 	}
 	
 	public double maxY() {
-		return this.center.getY() + this.height / 2;
+		return this.center.getY() + this.size.getHeight() / 2;
 	}
 	
-	public void setCenter(Location center, boolean replace) {
-		if (replace)
-			this.center = center;
-		else
-			this.center.setX(center.getX()).setY(center.getY());
-	}
-	
-	public void setCenter(Location center) {
-		this.setCenter(center, false);
-	}
-	
-	public void setCenter(double x, double y, boolean replace) {
-		if (replace)
-			this.center = new Location(x, y);
-		else
-			this.center.setX(x).setY(y);
-	}
-	
-	public void setCenter(double x, double y) {
-		this.setCenter(x, y, false);
-	}
-	
-	public double getWidth() {
-		return this.width;
-	}
-	
-	public double getHeight() {
-		return this.height;
+	public Size getSize() {
+		return this.size;
 	}
 	
 	public boolean equals(Hitbox other) {
-		return this.center.equals(other.getCenter())
-				&& this.width == other.getWidth()
-				&& this.height == other.getHeight();
+		return this.center.equals(other.getCenter()) && this.size.equals(other.getSize());
 	}
 
 	@Override
 	public String toString() {
-		return "Hitbox [center=" + center + ", width=" + width + ", height=" + height + "]";
+		return "Hitbox [center=" + center + ", size=" + size + "]";
+	}
+	
+	public static class Size {
+		private double width;
+		private double height;
+		
+		public Size(double width, double height) {
+			this.width = width;
+			this.height = height;
+		}
+		
+		public double getVolume() {
+			return this.width * this.height;
+		}
+
+		public double getWidth() {
+			return this.width;
+		}
+		
+		public double getHeight() {
+			return this.height;
+		}
+		
+		public boolean equals(Size other) {
+			return this.width == other.getWidth()
+					&& this.height == other.getHeight();
+		}
+
+		@Override
+		public String toString() {
+			return "Size [width=" + width + ", height=" + height + "]";
+		}
 	}
 }
