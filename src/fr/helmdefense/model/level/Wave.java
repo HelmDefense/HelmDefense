b2b1fbc67a027c841f186422884df2f8e4b7786d
indyteo <theo.mail@laposte.net>
2020-05-24 16:34:27 +0200

    Makes waves fully functionnal

--- src/fr/helmdefense/model/level/Wave.java
+++ src/fr/helmdefense/model/level/Wave.java
@@ -4,23 +4,37 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import fr.helmdefense.model.actions.ActionHandler;
+import fr.helmdefense.model.actions.ActionListener;
+import fr.helmdefense.model.actions.entity.living.LivingEntityDeathAction;
+import fr.helmdefense.model.actions.game.GameTickAction;
+import fr.helmdefense.model.actions.utils.Actions;
 import fr.helmdefense.model.entities.Entity;
+import fr.helmdefense.model.entities.abilities.Ability;
 import fr.helmdefense.model.entities.utils.Entities;
+import fr.helmdefense.model.entities.utils.Tier;
 
-public class Wave {
-	private int duration;
+public class Wave implements ActionListener {
 	private int reward;
-	private Map<Integer, Entity> entities;
+	private Map<Long, Entity> entities;
+	private int entityCount;
+	private Level lvl;
+	private long startTick;
+	private long endTick;
 	
-	public Wave(int duration, int reward, Map<Integer, String> entities) {
-		this.duration = duration;
+	public static final long TICKS_BEFORE_FIRST_WAVE = 100;
+	public static final long TICKS_BETWEEN_EACH_WAVE = 50;
+	
+	public Wave(int reward, Map<Integer, String> entities) {
 		this.reward = reward;
 		this.entities = entities.entrySet().stream()
 				.collect(Collectors.toMap(
-						e -> e.getKey(),
+						e -> ((Number) e.getKey()).longValue(),
 						e -> {
 							try {
-								return Entities.getClass("attackers." + e.getValue()).getConstructor(double.class, double.class).newInstance(0, 0);
+								Entity entity = Entities.getClass("attackers." + e.getValue()).getConstructor(double.class, double.class).newInstance(0, 0);
+								entity.addAbilities(new WaveDeathCountAbility());
+								return entity;
 							} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
 									| InvocationTargetException | NoSuchMethodException | SecurityException e1) {
 								e1.printStackTrace();
@@ -28,18 +42,69 @@ public class Wave {
 							}
 						}
 				));
+		this.entityCount = this.entities.size();
+		this.lvl = null;
+		this.startTick = -1;
+		this.endTick = -1;
 	}
 	
-	public int getDuration() {
-		return this.duration;
+	public void start(Level lvl) {
+		if (this.lvl != null)
+			return;
+		this.lvl = lvl;
+		
+		System.out.println("A wave started");
+		
+		Actions.registerListeners(this);
+	}
+	
+	@ActionHandler
+	public void onTick(GameTickAction action) {
+		if (this.startTick == -1)
+			this.startTick = action.getTicks();
+		
+		long ticks = action.getTicks() - this.startTick;
+		Entity e = this.entities.get(ticks);
+		if (e != null) {
+			e.teleport(this.lvl.getMap().getSpawn());
+			e.spawn(this.lvl);
+		}
+		
+		if (this.isEnded()) {
+			this.lvl.earnCoins(this.reward);
+			this.endTick = action.getTicks();
+			
+			System.out.println("A wave ended");
+			
+			Actions.unregisterListeners(this);
+		}
 	}
 	
 	public int getReward() {
 		return this.reward;
 	}
+	
+	public boolean isEnded() {
+		return this.entityCount == 0;
+	}
+	
+	public long getEndTick() {
+		return this.endTick;
+	}
 
 	@Override
 	public String toString() {
-		return "Wave [duration=" + duration + ", reward=" + reward + ", entities=" + entities + "]";
+		return "Wave [reward=" + reward + ", entities=" + entities + "]";
+	}
+	
+	public class WaveDeathCountAbility extends Ability {
+		public WaveDeathCountAbility() {
+			super(Tier.TIER_0);
+		}
+		
+		@ActionHandler
+		public void onDeath(LivingEntityDeathAction action) {
+			entityCount--;
+		}
 	}
 }
