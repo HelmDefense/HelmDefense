64b869c5915638dbe602e370bc384904768f8be8
indyteo <theo.mail@laposte.net>
2020-05-26 19:55:07 +0200

    Added statExtractor to Attribute

--- src/fr/helmdefense/model/entities/utils/Attribute.java
+++ src/fr/helmdefense/model/entities/utils/Attribute.java
@@ -1,10 +1,22 @@
 package fr.helmdefense.model.entities.utils;
 
+import java.util.function.Function;
+
 public enum Attribute {
-	HP,
-	DMG,
-	MVT_SPD,
-	ATK_SPD,
-	ATK_RANGE,
-	SHOOT_RANGE;
+	HP(s -> (double) s.getHp()),
+	DMG(s -> (double) s.getDmg()),
+	MVT_SPD(s -> s.getMvtSpd()),
+	ATK_SPD(s -> s.getAtkSpd()),
+	ATK_RANGE(s -> s.getAtkRange()),
+	SHOOT_RANGE(s -> s.getShootRange());
+	
+	private Function<Statistic, Double> statExtractor;
+	
+	private Attribute(Function<Statistic, Double> statExtractor) {
+		this.statExtractor = statExtractor;
+	}
+	
+	public double get(Statistic stat) {
+		return this.statExtractor.apply(stat);
+	}
 }
