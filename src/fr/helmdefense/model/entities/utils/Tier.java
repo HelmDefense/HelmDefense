584b128727b4c9550f6faa3eb977dd61ea619a16
indyteo <theo.mail@laposte.net>
2020-05-27 19:31:13 +0200

    Added DEFAULT in Tier and sued it as default & Added Tier.Specification enum

--- src/fr/helmdefense/model/entities/utils/Tier.java
+++ src/fr/helmdefense/model/entities/utils/Tier.java
@@ -1,34 +1,61 @@
 package fr.helmdefense.model.entities.utils;
 
 public enum Tier {
-	TIER_1(1),
-	TIER_2(2),
-	TIER_3(3);
+	TIER_1(1, false),
+	TIER_2(2, false),
+	TIER_3(3, true);
 	
 	private int n;
+	private boolean specification;
 	
-	private Tier(int n) {
+	public static final Tier DEFAULT = TIER_1;
+	
+	private Tier(int n, boolean specification) {
 		this.n = n;
+		this.specification = specification;
+	}
+	
+	public Tier previous() {
+		Tier previous = null;
+		for (Tier t : values()) {
+			if (t == this)
+				return previous;
+			previous = t;
+		}
+		return null;
 	}
 	
 	public Tier next() {
 		boolean found = false;
-		for(Tier t : values()) {
-			if(found)
+		for (Tier t : values()) {
+			if (found)
 				return t;
-			if(t == this)
+			if (t == this)
 				found = true;
 		}
 		return null;
 	}
 	
+	public boolean hasSpecification() {
+		return this.specification;
+	}
+	
 	public static Tier number(int n) {
-		if(n < 1 || n > 3)
+		if (n < 1 || n > 3)
 			return null;
 		return valueOf("TIER_" + n);
 	}
 	
+	@Override
 	public String toString() {
 		return "Tier " + this.n;
+	}
+	
+	public static enum Specification {
+		NO_SPECIFICATION,
+		A,
+		B;
+		
+		public static final Specification DEFAULT = NO_SPECIFICATION;
 	}
 }
