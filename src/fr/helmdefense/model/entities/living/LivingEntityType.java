cabe67f661e5de38ab50c84f60b1941fc1907039
TelioBOT <teliobot@gmail.com>
2020-06-10 10:51:42 +0200

    added door entity

--- src/fr/helmdefense/model/entities/living/LivingEntityType.java
+++ src/fr/helmdefense/model/entities/living/LivingEntityType.java
@@ -17,12 +17,12 @@ public enum LivingEntityType implements EntityType {
 	CATAPULT(EntitySide.DEFENDER),
 	
 	// Heroes
-	ARAGORN(EntitySide.DEFENDER, false),
-	LEGOLAS(EntitySide.DEFENDER, false),
-	GIMLI(EntitySide.DEFENDER, false),
+	ARAGORN(EntitySide.DEFENDER, SubType.HERO),
+	LEGOLAS(EntitySide.DEFENDER, SubType.HERO),
+	GIMLI(EntitySide.DEFENDER, SubType.HERO),
 	
 	//Door
-	DOOR(EntitySide.DEFENDER),
+	DOOR(EntitySide.DEFENDER, SubType.SPECIAL),
 	
 	/*** Attackers ***/
 	// Classic
@@ -33,37 +33,37 @@ public enum LivingEntityType implements EntityType {
 	TROLL(EntitySide.ATTACKER),
 	
 	// Bosses
-	ISENGARD_TOWER(EntitySide.ATTACKER, false),
-	MORDOR_TOWER(EntitySide.ATTACKER, false),
-	GROND(EntitySide.ATTACKER, false),
-	SARUMAN(EntitySide.ATTACKER, false),
-	ANGMAR_WITCH_KING(EntitySide.ATTACKER, false),
-	NAZGUL(EntitySide.ATTACKER, false);
+	ISENGARD_TOWER(EntitySide.ATTACKER, SubType.BOSS),
+	MORDOR_TOWER(EntitySide.ATTACKER, SubType.BOSS),
+	GROND(EntitySide.ATTACKER, SubType.BOSS),
+	SARUMAN(EntitySide.ATTACKER, SubType.BOSS),
+	ANGMAR_WITCH_KING(EntitySide.ATTACKER, SubType.BOSS),
+	NAZGUL(EntitySide.ATTACKER, SubType.BOSS);
 	
 	private EntitySide side;
-	private boolean classic;
+	private SubType subType;
 	private EntityData data;
 
-	public static final LivingEntityType[] CLASSIC_DEFENDERS = get(EntitySide.DEFENDER, true);
-	public static final LivingEntityType[] CLASSIC_ATTACKERS = get(EntitySide.ATTACKER, true);
-	public static final LivingEntityType[] HEROES = get(EntitySide.DEFENDER, false);
-	public static final LivingEntityType[] BOSSES = get(EntitySide.ATTACKER, false);
+	public static final LivingEntityType[] CLASSIC_DEFENDERS = get(EntitySide.DEFENDER, SubType.CLASSIC);
+	public static final LivingEntityType[] CLASSIC_ATTACKERS = get(EntitySide.ATTACKER, SubType.CLASSIC);
+	public static final LivingEntityType[] HEROES = get(EntitySide.DEFENDER, SubType.HERO);
+	public static final LivingEntityType[] BOSSES = get(EntitySide.ATTACKER, SubType.BOSS);
 	
 	private LivingEntityType(EntitySide side) {
-		this(side, true);
+		this(side, SubType.CLASSIC);
 	}
 	
-	private LivingEntityType(EntitySide side, boolean classic) {
+	private LivingEntityType(EntitySide side, SubType subType) {
 		this.side = side;
-		this.classic = classic;
+		this.subType = subType;
 	}
 	
 	public EntitySide getSide() {
 		return this.side;
 	}
 	
-	public boolean isClassic() {
-		return this.classic;
+	public SubType getSubType() {
+		return this.subType;
 	}
 	
 	@Override
@@ -88,10 +88,17 @@ public enum LivingEntityType implements EntityType {
 		return list.toArray(new LivingEntityType[list.size()]);
 	}
 	
-	public static LivingEntityType[] get(EntitySide side, boolean classic) {
+	public static LivingEntityType[] get(EntitySide side, SubType subType) {
 		List<LivingEntityType> list = Arrays.stream(values())
-				.filter(type -> type.getSide() == side && type.isClassic() == classic)
+				.filter(type -> type.getSide() == side && type.getSubType() == subType)
 				.collect(Collectors.toList());
 		return list.toArray(new LivingEntityType[list.size()]);
+	}
+	
+	public enum SubType {
+		CLASSIC,
+		HERO,
+		BOSS,
+		SPECIAL;
 	}
 }
--- src/fr/helmdefense/model/entities/living/special/Door.java
+++ src/fr/helmdefense/model/entities/living/special/Door.java
@@ -1,51 +1,70 @@
 package fr.helmdefense.model.entities.living.special;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import fr.helmdefense.model.actions.ActionHandler;
 import fr.helmdefense.model.actions.entity.living.LivingEntityDeathAction;
 import fr.helmdefense.model.actions.game.GameLooseAction;
 import fr.helmdefense.model.actions.game.GameTickAction;
+import fr.helmdefense.model.actions.utils.Actions;
 import fr.helmdefense.model.entities.Entity;
+import fr.helmdefense.model.entities.abilities.Ability;
 import fr.helmdefense.model.entities.living.LivingEntity;
 import fr.helmdefense.model.entities.living.LivingEntityType;
+import fr.helmdefense.model.entities.utils.Tier;
 import fr.helmdefense.model.entities.utils.coords.Location;
+import fr.helmdefense.model.level.Level;
 
 public class Door extends LivingEntity {
-	private double effectRange;
-	private int hp;
 	private boolean isFinalDoor;
-	
-	public Door(LivingEntityType type, Location loc, Integer hp, Boolean isFinalDoor) {
-		this(type, loc, 1d, hp, isFinalDoor);
-	}
+	private List<LivingEntity> entityMarked;
 
-	public Door(LivingEntityType type, Location loc, Double effectRange, Integer hp, Boolean isFinalDoor) {
+	public Door(LivingEntityType type, Location loc, int hp, Level lvl, boolean isFinalDoor) {
 		super(type, loc);
-		this.effectRange = effectRange;
-		this.hp = hp;
+		this.setHp(hp);
 		this.isFinalDoor = isFinalDoor;
+		this.entityMarked = new ArrayList<LivingEntity>();
+		this.addAbilities(new DeathAbility());
+		
+		this.spawn(lvl);
 	}
 	
 	@ActionHandler
-	public void onTick(GameTickAction action) {
+	public void setImmobile(GameTickAction action) {
 		for (Entity entity : action.getLvl().getEntities()) {
 			if (entity instanceof LivingEntity
-					&& entity.getLoc().distance(this.getLoc()) < this.effectRange) {
+					&& entity.getHitbox().overlaps(this.getHitbox())) {
 				((LivingEntity) entity).addFlags(LivingEntity.IMMOBILE);
+				entityMarked.add((LivingEntity) entity);
 			}
 		}
 	}
 	
-	@ActionHandler
-	public void onEntityDeath(LivingEntityDeathAction action) {
-		for (Entity entity : action.getEntity().getLevel().getEntities()) {
-			if (entity instanceof LivingEntity
-					&& entity.getLoc().distance(this.getLoc()) < this.effectRange) {
-				((LivingEntity) entity).removeFlags(LivingEntity.IMMOBILE);
+	public class DeathAbility extends Ability {
+		
+		public DeathAbility() {
+			super(Tier.DEFAULT, Tier.Specification.DEFAULT);
+		}
+
+		@ActionHandler
+		public void onEntityDeath(LivingEntityDeathAction action) {
+			Iterator<LivingEntity> it = entityMarked.iterator();
+			while (it.hasNext()) {
+				LivingEntity entity = it.next();
+				entity.removeFlags(LivingEntity.IMMOBILE);
+				it.remove();
+			}
+			if (isFinalDoor) {
+				GameLooseAction looseAction = new GameLooseAction(getLevel());
+				
+				getLevel().end();
+				
+				Actions.trigger(looseAction);
 			}
 		}
-		if (isFinalDoor) {
-			action.getEntity().triggerAbilities(new GameLooseAction(action.getEntity().getLevel()));
-		}
+		
 	}
 	
 }
